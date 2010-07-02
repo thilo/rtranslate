62523ae27d03327c86a6ea482427b977d170f39b
Adam Singer <adam@artforge.com>
2010-07-02 12:53:10 -0400

    Fix tests with correct translations Add post methods to test

--- tests/ts_translate.rb
+++ tests/ts_translate.rb
@@ -6,9 +6,10 @@ require 'rtranslate'
 
 class Translate::TranslateTest < Test::Unit::TestCase
   include Google::Language
+  
   def test_english_translate
     assert_equal("مرحبا العالم", Translate.t("Hello world", "ENGLISH", "ARABIC"));
-    assert_equal("世界您好", Translate.t("Hello world", "ENGLISH", "CHINESE_SIMPLIFIED"));
+    assert_equal("您好世界", Translate.t("Hello world", "ENGLISH", "CHINESE_SIMPLIFIED"));
     assert_equal("Bonjour tout le monde", Translate.t("Hello world", "ENGLISH", "FRENCH"));
     assert_equal("Hallo Welt", Translate.t("Hello world", "ENGLISH", "GERMAN"));
     assert_equal("Ciao a tutti", Translate.t("Hello world", "ENGLISH", "ITALIAN"));
@@ -21,7 +22,7 @@ class Translate::TranslateTest < Test::Unit::TestCase
 
   def test_auto_detect_translate
     assert_equal("مرحبا العالم", Translate.t("Hello world", nil, "ARABIC"));
-    assert_equal("世界您好", Translate.t("Hello world", nil, "CHINESE_SIMPLIFIED"));
+    assert_equal("您好世界", Translate.t("Hello world", nil, "CHINESE_SIMPLIFIED"));
     assert_equal("Bonjour tout le monde", Translate.t("Hello world", nil, "FRENCH"));
     assert_equal("Hallo Welt", Translate.t("Hello world", nil, "GERMAN"));
     assert_equal("Ciao a tutti", Translate.t("Hello world", nil, "ITALIAN"));
@@ -33,26 +34,42 @@ class Translate::TranslateTest < Test::Unit::TestCase
   end
 
   def test_chinese_translate
-    assert_equal("Hello World", Translate.t("你好世界", "CHINESE", "ENGLISH"))
-    assert_equal("Hello World", Translate.t("你好世界", 'zh', 'en'))
+    assert_equal("Hello World", Translate.t("您好世界", "CHINESE", "ENGLISH"))
+    assert_equal("Hello World", Translate.t("您好世界", 'zh', 'en'))
   end
 
   def test_unsupported_translate
     assert_raise UnsupportedLanguagePair do
-      Translate::RTranslate.t("你好世界", 'zh', 'hz')
+      Translate::RTranslate.t("您好世界", 'zh', 'hz')
     end
   end
 
   def test_translate_strings
-    assert_equal(["喂", "世界"], Translate::RTranslate.translate_strings(["Hello", "World"],  "en", "zh-CN"))
+    assert_equal(["你好", "世界"], Translate::RTranslate.translate_strings(["Hello", "World"],  "en", "zh-CN"))
   end
 
   def test_translate_string_to_languages
-    assert_equal(["世界您好", "ハローワールド"], Translate::RTranslate.translate_string_to_languages("Hello World", {:from => "en", :to => ["zh-CN", "ja"]}))
+    assert_equal(["您好世界", "ハローワールド"], Translate::RTranslate.translate_string_to_languages("Hello World", {:from => "en", :to => ["zh-CN", "ja"]}))
   end
 
   def test_batch_translate
-    assert_equal(["世界您好", "ハローワールド"],
-                 Translate::RTranslate.batch_translate([["Hello World", {:from => "en", :to => "zh-CN"}], ["Hello World", {:from => "en", :to => "ja"}]]))
+    assert_equal(["您好世界", "ハローワールド"],
+        Translate::RTranslate.batch_translate([["Hello World", {:from => "en", :to => "zh-CN"}], ["Hello World", {:from => "en", :to => "ja"}]]))
+  end
+  
+  def test_post_translate
+    # Basic
+    assert_equal("مرحبا العالم", Translate.t("Hello world", "ENGLISH", "ARABIC", {:method => :post}));
+    # Auto Detect Language
+    assert_equal("您好世界", Translate.t("Hello world", nil, "CHINESE_SIMPLIFIED", {:method => :post}));
+    #Chinese
+    assert_equal("Hello World", Translate.t("您好世界", "CHINESE", "ENGLISH", {:method => :post}))
+    #Strings
+    assert_equal(["你好", "世界"], Translate::RTranslate.translate_strings(["Hello", "World"],  "en", "zh-CN", {:method => :post}))
+    # String to Languages
+    assert_equal(["您好世界", "ハローワールド"], Translate::RTranslate.translate_string_to_languages("Hello World", {:from => "en", :to => ["zh-CN", "ja"], :method => :post}))
+    # Batch
+    assert_equal(["您好世界", "ハローワールド"],        
+      Translate::RTranslate.batch_translate([["Hello World", {:from => "en", :to => "zh-CN"}], ["Hello World", {:from => "en", :to => "ja"}]], {:method => :post}))
   end
 end
